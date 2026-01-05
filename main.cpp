49c469863c571399ef6aa006bca7eff9288eee75
wahyuuuwid <idwahyu58@gmail.com>
2026-01-05 11:20:14 +0700

    FIX Update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "./include/auth.hpp"
 #include "./include/posts.hpp"
+#include "./include/admin.hpp"
+
 using namespace std;
 
 void menuTimeLine(addrUser rootUser) {
@@ -8,13 +10,14 @@ void menuTimeLine(addrUser rootUser) {
     
     do {
         cout << "\n=== TIMELINE GLOBAL ===\n";
-        if (rootUser == NULL || rootUser->firstPost == NULL) {
+        if (rootUser == NULL) {
             cout << "Timeline masih kosong. Belum ada postingan." << endl;
             break;
         } else {
             showTimeline(rootUser);
         }
 
+        cout << endl;
         cout << "1. Like\n";
         cout << "0. Kembali\n";
         cout << "Pilih: ";
@@ -45,18 +48,19 @@ void menuUser(addrUser currentUser, addrUser rootUser) {
         cout << "1. Buat Post\n";
         cout << "2. Lihat Post Saya\n";
         cout << "3. Lihat Timeline Global\n";
-        cout << "4. Edit Post\n";
-        cout << "5. Hapus Post\n";
+        cout << "4. Lihat profil\n";
+        cout << "5. Temukan Pengguna lain\n";
         cout << "0. Logout\n";
         cout << "Pilih: ";
         cin >> postChoice;
 
         switch (postChoice) {
+
             case 1: {
                 string content;
                 cin.ignore();
 
-                cout << "Masukkan Isi Post  : ";
+                cout << "Masukkan Isi Post: ";
                 getline(cin, content);
 
                 addrPost newPost = createPost(content);
@@ -66,24 +70,183 @@ void menuUser(addrUser currentUser, addrUser rootUser) {
                 break;
             }
 
-            case 2:
-                cout << "\n=== Postingan saya ===\n";
-                printPosts(currentUser);
+            case 2: {
+                int postMenu;
+
+                do {
+                    cout << "\n=== Postingan saya ===\n";
+                    printPosts(currentUser);
+                    
+                    cout << endl;
+                    cout << "1. Hapus Post\n";
+                    cout << "2. Edit Post\n";
+                    cout << "3. Like post\n";
+                    cout << "0. Kembali\n";
+
+                    cout << "Pilih: ";
+                    cin >> postMenu;
+
+                    switch (postMenu){
+                        case 1:
+                            deletePost(currentUser);
+                            break;
+                        case 2:
+                            updatePost(currentUser);
+                            break;
+                        case 3:
+                            int postId;
+                            cout << "Masukkan ID post yang ingin di-like: ";
+                            cin >> postId;
+                            likePost(rootUser, postId);
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            cout << "Pilihan tidak valid.\n";
+                    }
+                } while (postMenu != 0);
                 break;
-            
+            }
+
             case 3:
                 menuTimeLine(rootUser);
                 break;
-            
-            case 4: {
-                cout << "\n=== Edit Post ===\n";
-                updatePost(currentUser);
+
+            case 4:{
+                int profileMenu;
+                do{
+                    printUserProfile(currentUser);
+
+                    cout << "1. Update Password\n";
+                    cout << "2. Update Bio\n";
+                    cout << "3. Update Interests\n";
+                    cout << "0. Kembali\n";
+                    cout << "Pilih: ";
+                    cin >> profileMenu;
+
+                    switch(profileMenu){
+                        case 1: {
+                            string pass, confirm;
+                            cout << "Password baru: ";
+                            cin >> pass;
+                            cout << "Konfirmasi password: ";
+                            cin >> confirm;
+                            
+                            if (updatePassword(currentUser, pass, confirm)){
+                                cout << "Password berhasil diupdate.\n";
+                            } else {
+                                cout << "Password gagal diupdate\n";
+                            }
+                            break;
+                        }
+
+                        case 2:{
+                            string newBio;
+                            cin.ignore();
+                            cout << "Bio baru: ";
+                            getline(cin, newBio);
+
+                            if (updateBio(currentUser, newBio)){
+                                cout << "Bio berhasil diperbarui.\n";
+                            } else {
+                                cout << "User tidak ditemukan.\n";
+                            }
+                            break;
+                        }
+                        
+                        case 3: {
+                            string newInterest[3];
+                            for (int i = 0; i<3; i++){
+                                cout << "Interest ke-" << i+1 << ": ";
+                                cin >> newInterest[i];
+                            }
+
+                            if (updateInterest(currentUser, newInterest)){
+                                cout << "Interest berhasil diperbarui.\n";
+                            } else {
+                                cout << "Interest gagal diperbarui.\n";
+                            }
+                            break;
+                        }
+
+                        case 0:
+                            break;
+                        default:
+                            cout << "Pilihan tidak valid.\n";
+                    }
+                } while (profileMenu !=0);
                 break;
             }
 
             case 5: {
-                cout << "\n=== Hapus Post ===\n";
-                deletePost(currentUser);
+                string usernameDicari;
+                int postOption;
+
+                cout << "Masukkan username yang akan dicari: ";
+                cin >> usernameDicari;
+                cout << endl;
+
+                addrUser foundUser = searchUser(rootUser, usernameDicari);
+                    
+                if (foundUser == nullptr){
+                    cout << "Pengguna dengan username: " << usernameDicari << " Tidak ditemukan.\n";
+                    break;
+                }
+
+                cout << "Pengguna dengan username: " << foundUser->info.username << " ditemukan!\n";
+                cout << endl;    
+
+                do {
+                    cout << "1. Lihat Profile\n";
+                    cout << "2. Lihat Postingan\n";
+                    cout << "0. Kembali\n";
+                    
+                    cout << "pilih: ";
+                    cin >> postOption;
+
+                    switch (postOption) {
+                        case 1:
+                            printUserProfile(foundUser);
+                            break;
+                        case 2:{
+                            printPosts(foundUser);
+                            int option;
+                            
+                            do{
+                                cout << "1. Like Postingan\n";
+                                cout << "0. Kembali\n";
+                                cout << "Pilih: ";
+                                cin >> option;
+
+                                switch(option){
+                                    case 1:
+                                        int postId;
+                                        cout << "Masukkan id postingan: ";
+                                        cin >> postId;
+
+                                        if(!(likePost(foundUser, postId))){
+                                            cout << "Like gagal.\n";
+                                            break;
+                                        }
+
+                                        break;
+                                    case 0:
+                                        cout << "Kembali...\n";
+                                        break;
+                                    default:
+                                        cout << "Pilihan tidak valid.\n";
+                                }
+                            } while(option != 0);
+
+                            break;
+                        } 
+                        case 0:
+                            cout << "Kembali...\n";
+                            break;
+                        default:
+                            cout << "Pilihan tidak valid.\n";
+                    }
+                } while(postOption != 0);
                 break;
             }
 
@@ -93,10 +256,52 @@ void menuUser(addrUser currentUser, addrUser rootUser) {
 
             default:
                 cout << "Pilihan tidak valid.\n";
-                break;
         }
+        
     } while (postChoice != 0);
 
+}
+
+void menuAdmin(addrUser rootUser){
+    int adminChoice;
+
+    do {
+        cout << "\n===== DASHBOARD ADMIN =====\n";
+        cout << "Total User        : " << countUsers(rootUser) << endl;
+        cout << "Total Post        : " << countTotalPosts(rootUser) << endl;
+        cout << endl;
+
+        cout << "Statistik Postingan:\n";
+        cout << "----------------------";
+        minPostLike(rootUser);
+        maxPostLike(rootUser);
+
+        cout << "\n=== MENU ADMIN ===\n";
+        cout << "1. Hapus User\n";
+        cout << "0. Logout\n";
+        cout << "Pilih: ";
+        cin >> adminChoice;
+
+        switch (adminChoice) {
+            case 1: {
+                cout << "Masukkan username yang akan dihapus: ";
+                string usernameHapus;
+                cin >> usernameHapus;
+                if (deleteUser(rootUser, usernameHapus)) {
+                    cout << "User berhasil dihapus.\n";
+                } else {
+                    cout << "User tidak ditemukan.\n";
+                }
+                break;
+            }
+            case 0:
+                cout << "Logout berhasil.\n";
+                break;
+            default:
+                cout << "Pilihan tidak valid.\n";
+        }
+
+    } while (adminChoice != 0);
 }
 
 int main() {
@@ -106,10 +311,9 @@ int main() {
     int choice;
 
     do {
-        cout << "\n=== TEST REGISTER & LOGIN ===\n";
+        cout << "\n=== REGISTER & LOGIN ===\n";
         cout << "1. Register\n";
         cout << "2. Login\n";
-        cout << "3. Lihat jumlah user (test)\n";
         cout << "0. Keluar\n";
         cout << "Pilih: ";
         cin >> choice;
@@ -122,13 +326,18 @@ int main() {
             case 2:
                 currentUser = loginUser(root);
                 if (currentUser != NULL) {
-                    menuUser(currentUser, root);
+                    if (currentUser->info.username == "admin") {
+                        menuAdmin(root);
+                    } else {
+                        menuUser(currentUser, root);
+                    }
                 }
                 break;
 
-            case 3:
-                cout << "Total user: " << countUsers(root) << endl;
+            case 0:
                 break;
+            default:
+                cout << "Pilihan tidak valid.\n";
         }
 
     } while (choice != 0);
